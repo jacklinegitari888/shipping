27203a7cc0f216b8ff22246044bcb8f4d8ab16c7
jacklinegitari888 <101161799+jacklinegitari888@users.noreply.github.com>
2022-05-24 16:02:12 +0300

    CREATING TEMPLATES
    Created template in poll model

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
-from .views import get_time, hello
+from .views import get_time, get_questions,get_detail,get_results,get_vote,get_question
 from django.urls import path
 
 urlpatterns = [
-   path("",hello),
-   path("time/",get_time)
+   path("questions/",get_questions),
+   path("questions/<int:id>",get_question),
+   path("time/",get_time),
+   path("detail/",get_detail),
+   path("results/",get_results),
+   path("vote/",get_vote)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,38 @@
+from http.client import responses
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+from .models import Question
+from django.template import loader
+
 
 # Create your views here.
 
 
-def hello(request):
-    return HttpResponse("hello world")
+def get_questions(request):
+    ## stuff 
+    questions=Question.objects.all()
+    template=loader.get_template("index.html")
+    context={"latest_question_list":questions}
+    return HttpResponse(template.render(context,request))
+
+def get_question(request,id):
+    question=Question.objects.get(pk=id)
+    if question:
+        template=loader.get_template("index.html")
+        context={"latest_question_list":[question]}
+        return HttpResponse(template.render(context,request))
+    return HttpResponse("Question with id "+str(id)+ "not available")
+    
 
 def get_time(request):
     now=f"it is now {time.localtime()}"     
     return HttpResponse(now)
+def get_detail(request, question_id):
+    return HttpResponse("you are looking at question %s." % question_id)
+def get_results(request, question_id):
+    response="you are looking at results of question %s "
+    return HttpResponse(response % question_id)
+def get_vote(request, question_id):
+    return HttpResponse("you are voting on question %s." % question_id)
+    
